3437469ab9c9138021200b8ea875440fdf4f2527
slobodanbrdar <slobodan.brdar11@gmail.com>
2019-11-21 21:43:06 +0100

    Model initialization

---
+++ NetworkModelService_Branch/NMSCommon/ModelDefines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSCommon
+{
+    public enum DMStype : short
+    {
+
+    }
+    
+    [Flags]
+    public enum ModelCode : long
+    {
+
+    }
+
+    [Flags]
+    public enum ModelCodeMask : long
+    {
+        MASK_TYPE = 0x00000000ffff0000,
+        MASK_ATTRIBUTE_INDEX = 0x000000000000ff00,
+        MASK_ATTRIBUTE_TYPE = 0x00000000000000ff,
+
+        MASK_INHERITANCE_ONLY = unchecked((long)0xffffffff00000000),
+        MASK_FIRSTNBL = unchecked((long)0xf000000000000000),
+        MASK_DELFROMNBL8 = unchecked((long)0xfffffff000000000),
+    }
+}
