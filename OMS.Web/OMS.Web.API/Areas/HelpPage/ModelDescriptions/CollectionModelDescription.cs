3192f5b79de242017f9c25492efba0bf19caefb3
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-12-02 21:19:10 +0100

    Initial commit

---
+++ OMS.Web/OMS.Web.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace OMS.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
