dc918989118876fe9b80b38084c7e439cc37b168
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-12-02 22:03:35 +0100

    Add initial persistence layer

---
+++ OMS.Web/OMS.Web.Persistence/OutageDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace OMS.Web.Persistence
+{
+    public class OutageDbContext : DbContext
+    {
+        public OutageDbContext(): base("name=OutageDbConnection")
+        {
+        }
+    }
+}
