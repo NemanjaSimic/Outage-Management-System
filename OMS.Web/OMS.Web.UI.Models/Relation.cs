0471909ddcfad2e8c195640f401d0e7b045b5328
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-12-05 19:46:38 +0100

    Add ui models

---
+++ OMS.Web/OMS.Web.UI.Models/Relation.cs
@@ -0,0 +1,15 @@
+﻿namespace OMS.Web.UI.Models
+{
+    /// <summary>
+    /// Represents a parent-child relation between two nodes.
+    /// Contains additional information about the relation.
+    /// </summary>
+    public class Relation
+    {
+        // These can be changed later on ...
+        public string SourceNodeId;
+        public string TargetNodeId;
+        public bool IsActive;
+        public bool IsAclLine; 
+    }
+}
