1de27276e669037bd52f5856d8ae7ba42b0cae87
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-12-05 20:27:48 +0100

    Add calculation engine wcf client mock

---
+++ OMS.Web/OMS.Web.Adapter.Mock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OMS.Web.Adapter.Mock.Client;
+
+namespace OMS.Web.Adapter.Mock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ClientProxy proxy = new ClientProxy("net.tcp://localhost:9990/WebService");
+
+            try
+            {
+                proxy.UpdateGraph(null, null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception occured during ClientProxy.UpdateGraph(): {e.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
