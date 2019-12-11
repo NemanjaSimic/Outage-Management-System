de7da3168d0c5da6366e51b96601054302f6b9aa
mladjo97 <mldnmilosevic@gmail.com>
2019-12-11 17:24:54 +0100

    Add tooltip button

--- OMS-Angular/src/app/shared/utils/tooltip.ts
+++ OMS-Angular/src/app/shared/utils/tooltip.ts
@@ -1,5 +1,4 @@
 import tippy from 'tippy.js';
-import 'tippy.js/themes/light-border.css';
 
 const graphTooltipBody: string =
 `<p>ID: [[id]]</p>
@@ -8,27 +7,36 @@ const graphTooltipBody: string =
 export const addGraphTooltip = (node) => {
   let ref = node.popperRef();
 
-  node.tippy = tippy(ref, {
+  node.tooltip = tippy(ref, {
     content: () => {
+      // node information - mozemo preko stringa da dodamo u div
       const div = document.createElement('div');
-
       div.innerHTML = graphTooltipBody
         .replace("[[id]]", node.data('id'))
         .replace("[[state]]", node.data('state'));
 
+      // button - mozemo i preko document.createElement() pa appendChild()
+      const button = document.createElement('button');
+      button.innerHTML = "Switch on";
+      button.addEventListener('click', () => {
+        console.log('switching on node with id: ', node.data('id'));
+      });
+
+      div.appendChild(button);
+      
       return div;
     },
-    theme: 'light-border',
     animation: 'perspective',
     trigger: 'manual',
     placement: 'right',
-    arrow: true
+    arrow: true,
+    interactive: true
   });
 
   node.on('tap', () => {
     // nemam pojma zasto ovako radi, ali radi ...
     setTimeout(() => {
-      node.tippy.show();
+      node.tooltip.show();
     }, 0);
   });
 }
