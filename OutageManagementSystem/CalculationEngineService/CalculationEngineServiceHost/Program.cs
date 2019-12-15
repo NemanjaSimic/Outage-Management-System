5e910bbe79a7edd598b86f96bb3f62cf01bbb0f9
Nemanja Simic <snemanja.simic@gmail.com>
2019-12-15 17:12:52 +0100

    Merged NMS and CE in one solution

---
+++ OutageManagementSystem/CalculationEngineService/CalculationEngineServiceHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using CECommon;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TopologyElementsFuntions;
+
+namespace CalculationEngineServiceHost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TopologyConnectivity topologyConnectivity = new TopologyConnectivity();
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			List<TopologyElement> energySources = topologyConnectivity.MakeAllTopologies();
+			stopwatch.Stop();
+			Console.WriteLine(stopwatch.Elapsed.ToString());
+			foreach (var rs in energySources)
+			{
+				topologyConnectivity.PrintTopology(rs);
+			}
+			Console.ReadLine();
+		}
+
+	}
+}
---
+++ OutageManagementSystem/CalculationEngineService/TopologyElementsFuntions/TopologyHelper.cs
@@ -0,0 +1,88 @@
+﻿using CECommon;
+using CECommon.TopologyConfiguration;
+using NetworkModelServiceFunctions;
+using Outage.Common;
+using System.Collections.Generic;
+
+namespace TopologyElementsFuntions
+{
+	public class TopologyHelper
+	{
+		private readonly Dictionary<TopologyStatus, List<DMSType>> elementsStatus;
+		private readonly Dictionary<TopologyType, List<DMSType>> topologyTypes;
+
+		private readonly GDAModelHelper gDAModelHelper = new GDAModelHelper();
+		private readonly ModelResourcesDesc modelResourcesDesc = new ModelResourcesDesc();
+
+		public TopologyHelper()
+		{
+			ConfigurationParse cp = new ConfigurationParse();
+			elementsStatus = cp.GetAllElementStatus();
+			topologyTypes = cp.GetAllTopologyTypes();
+		}
+
+		public TopologyStatus GetElementTopologyStatus(long gid)
+		{
+			TopologyStatus retVal;
+			DMSType type = ModelCodeHelper.GetTypeFromModelCode(modelResourcesDesc.GetModelCodeFromId(gid));
+			foreach (var item in elementsStatus)
+			{
+				if (item.Value.Contains(type))
+				{
+					retVal = item.Key;
+					return retVal;
+				}
+			}
+			return TopologyStatus.Regular;
+		}
+
+		public TopologyType GetElementTopologyType(long gid)
+		{
+			TopologyType retVal;
+			DMSType type = ModelCodeHelper.GetTypeFromModelCode(modelResourcesDesc.GetModelCodeFromId(gid));
+			foreach (var item in topologyTypes)
+			{
+				if (item.Value.Contains(type))
+				{
+					retVal = item.Key;
+					return retVal;
+				}
+			}
+			return TopologyType.None;
+		}
+
+		public List<long> GetAllReferencedElements(long gid)
+		{
+			List<long> elements = new List<long>();
+
+			foreach (var resourceDescription in gDAModelHelper.GetAllReferencedElements(gid))
+			{
+				elements.Add(resourceDescription.Id);
+			}
+
+			return elements;
+		}
+
+		public List<long> GetAllEnergySources() => gDAModelHelper.GetAllEnergySousces();
+
+		public string GetDMSTypeOfTopologyElement(long gid)
+		{
+			try
+			{
+				return ModelCodeHelper.GetTypeFromModelCode(modelResourcesDesc.GetModelCodeFromId(gid)).ToString();
+
+			}
+			catch (System.Exception)
+			{
+				if (gid < 5000)
+				{
+					return "EDGE";
+				}
+				else
+				{
+					return "FIELD";
+				}
+			}
+		}
+	}
+}
