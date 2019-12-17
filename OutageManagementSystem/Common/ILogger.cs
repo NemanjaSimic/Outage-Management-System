18f769e97da477ce9314945d7e93c914e50577ce
mladjo97 <mldnmilosevic@gmail.com>
2019-12-17 09:17:55 +0100

    Modify logger interface

--- OutageManagementSystem/Common/ILogger.cs
+++ OutageManagementSystem/Common/ILogger.cs
@@ -4,14 +4,14 @@ namespace Outage.Common
 {
     public interface ILogger
     {
-        void LogInfo(string message, Exception e);
+        void LogInfo(string message, Exception e = null);
 
-        void LogDebug(string message, Exception e);
+        void LogDebug(string message, Exception e = null);
 
-        void LogWarn(string message, Exception e);
+        void LogWarn(string message, Exception e = null);
 
-        void LogError(string message, Exception e);
+        void LogError(string message, Exception e = null);
 
-        void LogFatal(string message, Exception e);
+        void LogFatal(string message, Exception e = null);
     }
 }
