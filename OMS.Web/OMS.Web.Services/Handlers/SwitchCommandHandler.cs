ae3826f52c3451b8dac5a3348093734842321603
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-12-18 20:00:16 +0100

    Add scada service client proxy

--- OMS.Web/OMS.Web.Services/Handlers/SwitchCommandHandler.cs
+++ OMS.Web/OMS.Web.Services/Handlers/SwitchCommandHandler.cs
@@ -1,32 +1,52 @@
-﻿using MediatR;
-using OMS.Web.Services.Commands;
+﻿using System;
+using MediatR;
 using Outage.Common;
 using System.Threading;
 using System.Threading.Tasks;
+using OMS.Web.Adapter.Contracts;
+using OMS.Web.Services.Commands;
 
 namespace OMS.Web.Services.Handlers
 {
     public class SwitchCommandHandler : IRequestHandler<TurnOffSwitchCommand>, IRequestHandler<TurnOnSwitchCommand>
     {
         private readonly ILogger _logger;
+        private readonly IScadaClient _scadaClient;
 
-        public SwitchCommandHandler(ILogger logger)
+        public SwitchCommandHandler(ILogger logger, IScadaClient scadaClient)
         {
             _logger = logger;
+            _scadaClient = scadaClient;
         }
 
         public Task<Unit> Handle(TurnOffSwitchCommand request, CancellationToken cancellationToken)
         {
             _logger.LogDebug($"Sending {request.Command.ToString()} command to {request.Gid}");
-            // TODO: Implement logic for sending command
-
+            
+            try
+            {
+                _scadaClient.SendCommand(request.Gid, (int)request.Command);
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("SwitchCommandHandler failed on TurnOffSwitch handler.", e);
+            }
+            
             return null;
         }
 
         public Task<Unit> Handle(TurnOnSwitchCommand request, CancellationToken cancellationToken)
         {
             _logger.LogDebug($"Sending {request.Command.ToString()} command to {request.Gid}");
-            // TODO: Implement logic for sending command
+
+            try
+            {
+                _scadaClient.SendCommand(request.Gid, (int)request.Command);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("SwitchCommandHandler failed on TurnOnSwitch handler.", e);
+            }
 
             return null;
         }
