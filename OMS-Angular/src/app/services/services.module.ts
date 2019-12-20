06d5c093fab23f39bba0b836eb870e4551a6925f
mladjo97 <mldnmilosevic@gmail.com>
2019-12-20 15:55:43 +0100

    Add command service

--- OMS-Angular/src/app/services/services.module.ts
+++ OMS-Angular/src/app/services/services.module.ts
@@ -3,11 +3,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '@shared/shared.module';
 
 import { EnvironmentService } from './environment/environment.service';
+import { GraphService } from './notification/graph.service';
+import { CommandService } from './command/command.service';
 
 @NgModule({
   declarations: [],
   providers: [
-    EnvironmentService
+    EnvironmentService,
+    GraphService,
+    CommandService
   ],
   imports: [
     HttpClientModule,
