45c8ceb2292e85d2fee7a963d7c39ac34670d595
TheLastHop3 <39307359+TheLastHop3@users.noreply.github.com>
2019-12-26 15:50:51 +0100

    Update SCADAModel.cs

--- OutageManagementSystem/SCADA/SCADAService/SCADAModel.cs
+++ OutageManagementSystem/SCADA/SCADAService/SCADAModel.cs
@@ -1,4 +1,6 @@
 ﻿using Outage.Common.GDA;
+using Outage.SCADA.SCADA_Config_Data.Configuration;
+using Outage.SCADA.SCADA_Config_Data.Repository;
 using System;
 using System.Collections.Generic;
 
@@ -7,14 +9,23 @@ namespace Outage.SCADA.SCADAService
     public class SCADAModel
     {
         //TODO: stanje "modela" -> npr string putanje ka dokumentu
-
+        private DataModelRepository scadaModel = DataModelRepository.Instance;
+        private Dictionary<DeltaOpType, List<long>> modelChanges;
+        private Dictionary<long, ResourceDescription> delta_NMS_Model;
+        private Dictionary<long, ConfigItem> delta_Points;
         public bool Notify(Dictionary<DeltaOpType, List<long>> modelChanges)
         {
-            throw new NotImplementedException();
+            this.modelChanges = modelChanges;
+            return true;
         }
 
         public bool Prepare()
         {
+            //TO DO:get inserted and updated RD and remove removed from collections
+            delta_NMS_Model = new Dictionary<long, ResourceDescription>(scadaModel.NMS_Model);
+            delta_Points = new Dictionary<long, ConfigItem>(scadaModel.Points);
+
+            
             throw new NotImplementedException();
         }
 
