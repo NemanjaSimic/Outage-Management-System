0cc564106867c3ce11a80632776984bf163fc882
Mladen Milosevic <mldnmilosevic@gmail.com>
2020-01-21 19:36:28 +0100

    Add imap email client

---
+++ OMS.EmailService/OMS.EmailService/Imap/ImapMailClient.cs
@@ -0,0 +1,35 @@
+﻿using ImapX;
+using System;
+using System.Configuration;
+
+namespace OMS.EmailService.Imap
+{
+    public class ImapMailClient : IEmailClient
+    {
+        private readonly ImapClient _client;
+
+        private readonly string _address;
+        private readonly string _password;
+        private readonly string _server;
+        private readonly int _port;
+
+        public ImapMailClient()
+        {
+            _address = ConfigurationManager.AppSettings["emailAddress"];
+            _password = ConfigurationManager.AppSettings["emailPassword"];
+            _server = ConfigurationManager.AppSettings["emailServer"];
+            _port = Int32.Parse(ConfigurationManager.AppSettings["emailPort"]);
+
+            _client = new ImapClient(_server, _port, true);
+        }
+
+        public bool Connect()
+        {
+            if (_client.Connect())
+                if (_client.Login(_address, _password))
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ OMS.EmailService/OMS.EmailService/Program.cs
@@ -0,0 +1,23 @@
+﻿using OMS.EmailService.Imap;
+using System;
+
+namespace OMS.EmailService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmailClient emailClient = new ImapMailClient();
+            if (emailClient.Connect())
+            {
+                Console.WriteLine("Connected email client");
+            }
+            else
+            {
+                Console.WriteLine("Could not connect email client.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
