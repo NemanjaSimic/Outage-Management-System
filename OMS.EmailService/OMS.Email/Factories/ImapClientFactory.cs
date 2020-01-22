8dbd865ab42c6f8abb232e5b316241ac6f27ad68
mladjo97 <mldnmilosevic@gmail.com>
2020-01-22 16:54:50 +0100

    Add factories

---
+++ OMS.EmailService/OMS.Email/Factories/ImapClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using OMS.Email.Imap;
+
+namespace OMS.Email.Factories
+{
+    public class ImapClientFactory
+    {
+        public ImapEmailClient CreateClient() => 
+            new ImapEmailClient(new ImapEmailMapper());
+    }
+}
--- OMS.EmailService/OMS.EmailService/Program.cs
+++ OMS.EmailService/OMS.EmailService/Program.cs
@@ -1,10 +1,11 @@
-﻿#define IDLE_SCAN
-//#define MANUAL_SCAN
+﻿//#define IDLE_SCAN
+#define MANUAL_SCAN
 
 using System;
 using System.Collections.Generic;
 using OMS.Email.Dispatchers;
 using OMS.Email.EmailParsers;
+using OMS.Email.Factories;
 using OMS.Email.Imap;
 using OMS.Email.Interfaces;
 using OMS.Email.Models;
@@ -15,18 +16,11 @@ namespace OMS.EmailService
     {
         static void Main(string[] args)
         {
-            // todo: add factory for email clients
-            IImapEmailMapper mapper = new ImapEmailMapper();
-            IDispatcher dispatcher = new GraphHubDispatcher();
-            IEmailParser emailParser = new OutageEmailParser();
-
-            #if (IDLE_SCAN)
-            #region Idle scan
-
+            #if (IDLE_SCAN)          
             Console.WriteLine("Idle scanning starting...");
 
             // Use-case #1: Idle all-time listening to new messages
-            IIdleEmailClient idleEmailClient = new ImapIdleEmailClient(mapper, emailParser, dispatcher);
+            IIdleEmailClient idleEmailClient = new ImapIdleClientFactory().CreateClient();
 
             if (!idleEmailClient.Connect())
             {
@@ -42,31 +36,27 @@ namespace OMS.EmailService
             }
 
             Console.WriteLine("Idle scanning started.");
-
-
-            #endregion
             #endif
 
+
             #if (MANUAL_SCAN)
-            #region Manual scan
+            Console.WriteLine("Manual scan starting ...");
 
-                        Console.WriteLine("Manual scan starting ...");
-                        // Use-case #2: Manual scan for unread emails that can be setup as a cron job
-                        IEmailClient emailClient = new ImapEmailClient(mapper);
+            IEmailClient emailClient = new ImapClientFactory().CreateClient();
 
-                        if (!emailClient.Connect())
-                        {
-                            Console.WriteLine("Could not connect email client.");
-                            Console.ReadLine();
-                            return;
-                        }
-                        IEnumerable<OutageMailMessage> unreadMessages = emailClient.GetUnreadMessages();
+            if (!emailClient.Connect())
+            {
+                Console.WriteLine("Could not connect email client.");
+                Console.ReadLine();
+                return;
+            }
 
-                        foreach (var message in unreadMessages)
-                            Console.WriteLine(message);
+            IEnumerable<OutageMailMessage> unreadMessages = emailClient.GetUnreadMessages();
+
+            foreach (var message in unreadMessages)
+                Console.WriteLine(message);
                         
-                        Console.WriteLine("Manual scan finished.");
-            #endregion
+            Console.WriteLine("Manual scan finished.");
             #endif
 
             Console.ReadLine();
