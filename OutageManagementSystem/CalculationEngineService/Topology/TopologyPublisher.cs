0c77b7ce8991a2e26028e9392d1b08e0d4986348
slobodanbrdar <slobodan.brdar11@gmail.com>
2020-02-04 09:20:18 +0100

    port change

--- OutageManagementSystem/CalculationEngineService/Topology/TopologyPublisher.cs
+++ OutageManagementSystem/CalculationEngineService/Topology/TopologyPublisher.cs
@@ -40,7 +40,7 @@ namespace Topology
         public void TopologyToOMSConvertDelegate(List<IOutageTopologyModel> outageTopologyModels)
         {
             OMSModelMessage message = new OMSModelMessage(outageTopologyModels.First());
-            CalcualtionEnginePublication publication = new CalcualtionEnginePublication(Topic.OMS_MODEL, message);
+            CalculationEnginePublication publication = new CalculationEnginePublication(Topic.OMS_MODEL, message);
             try
             {
                 using (var publisherProxy = new PublisherProxy(EndpointNames.PublisherEndpoint))
