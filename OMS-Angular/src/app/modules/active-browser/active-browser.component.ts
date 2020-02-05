a79cbb6d793c49e6c1e0f13a671869c799d795ab
Mladen Milosevic <mldnmilosevic@gmail.com>
2020-02-05 20:04:59 +0100

    Add active outages table

--- OMS-Angular/src/app/modules/active-browser/active-browser.component.ts
+++ OMS-Angular/src/app/modules/active-browser/active-browser.component.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as outages from './outage-mock.json'
+import { ActiveOutage } from '@shared/models/outage.model';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+};
+
+const AO_MOCK: ActiveOutage[] = [
+  { Id: 1, ElementId: 2321619, DateCreated: new Date(), AfectedConsumers: [] },
+  { Id: 2, ElementId: 3311516, DateCreated: new Date(), AfectedConsumers: [] },
+  { Id: 3, ElementId: 4321512, DateCreated: new Date(), AfectedConsumers: [] },
+  { Id: 4, ElementId: 5684515, DateCreated: new Date(), AfectedConsumers: [] },
+  { Id: 5, ElementId: 6151715, DateCreated: new Date(), AfectedConsumers: [] },
+  { Id: 6, ElementId: 7236541, DateCreated: new Date(), AfectedConsumers: [] }
+];
 
 @Component({
   selector: 'app-active-browser',
@@ -9,12 +25,13 @@ import * as outages from './outage-mock.json'
 })
 
 export class ActiveBrowserComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'dateCreated'];
-  dataSource = outages;
+  private activeOutages: ActiveOutage[] = AO_MOCK;
+  private columns: string[] = ["id", "elementId", "dateCreated"];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.activeOutages);
   }
 
 }
