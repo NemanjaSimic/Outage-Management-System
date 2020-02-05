a62b5b7f2a955b18e38965fdec0b0a499508189b
Mladen Milosevic <mldnmilosevic@gmail.com>
2020-02-05 20:04:59 +0100

    Add active outages table

--- OMS-Angular/src/app/modules/routing/routes.declaration.ts
+++ OMS-Angular/src/app/modules/routing/routes.declaration.ts
@@ -1,20 +1,11 @@
 import { Route } from '@angular/router';
-// import { AppComponent } from 'app/app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { GraphComponent } from '@modules/graph/components/graph/graph.component';
-import { SplashComponent } from '@shared/components/splash/splash.component';
 import { ActiveBrowserComponent } from '@modules/active-browser/active-browser.component';
-// import { HistoricalBrowserComponent } from '@modules/historical-browser/historical-browser.component';
 
 export const rootRoutes: Route[] = [
   { path: '', component: GraphComponent },
   { path: 'graph', component: GraphComponent },
-  { path: 'splash', component: SplashComponent },
-<<<<<<< 3350cb88550753de3fb17c39460a84c4edcaeef5
   { path: 'active-browser', component: ActiveBrowserComponent },
-=======
-  { path: 'active_browser', component: ActiveBrowserComponent },
->>>>>>> WEB: outage query handling
-  // { path: 'historical_browser', component: HistoricalBrowserComponent },
   { path: '**', component: NotFoundComponent }
 ]
