530cfa62e38ed136d517a0a29ff87391d9181cb4
RedCrusaderJr <dimitrijemitic1996@gmail.com>
2020-02-05 16:46:18 +0100

    WEB: backend ViewModel ArchivedOutage

--- OMS.Web/OMS.Web.API/Controllers/OutageController.cs
+++ OMS.Web/OMS.Web.API/Controllers/OutageController.cs
@@ -1,4 +1,16 @@
+<<<<<<< 4922d294ad4802527c71ac4f94f01f3f712a64bb
 ﻿namespace OMS.Web.API.Controllers
+=======
+﻿using MediatR;
+using OMS.Web.Services.Queries;
+using OMS.Web.UI.Models.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace OMS.Web.API.Controllers
+>>>>>>> WEB: backend ViewModel ArchivedOutage
 {
     using MediatR;
     using OMS.Web.Services.Queries;
@@ -17,10 +29,18 @@
         }
 
         [HttpGet]
+<<<<<<< 4922d294ad4802527c71ac4f94f01f3f712a64bb
         [Route("api/outage/active")]
         public async Task<IHttpActionResult> GetActive()
         {
             IEnumerable<ActiveOutageViewModel> activeOutages = await _mediator.Send(new GetActiveOutagesQuery());
+=======
+        [ResponseType(typeof(IEnumerable<Outage.Common.ServiceContracts.OMS.ActiveOutage>))]
+        [Route("api/outage/getActive")]
+        public async Task<IHttpActionResult> GetActive()
+        {
+            IEnumerable<Outage.Common.ServiceContracts.OMS.ActiveOutage> activeOutages = await _mediator.Send<IEnumerable<Outage.Common.ServiceContracts.OMS.ActiveOutage>>(new GetActiveOutagesQuery());
+>>>>>>> WEB: backend ViewModel ArchivedOutage
             return Ok(activeOutages);
         }
 
