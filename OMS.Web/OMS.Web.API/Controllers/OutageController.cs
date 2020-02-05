1d3e6ad0eceee73d69ff09faca6eccad2fb27435
RedCrusaderJr <dimitrijemitic1996@gmail.com>
2020-02-05 16:56:35 +0100

    Post Merge modifications

--- OMS.Web/OMS.Web.API/Controllers/OutageController.cs
+++ OMS.Web/OMS.Web.API/Controllers/OutageController.cs
@@ -17,10 +17,18 @@
         }
 
         [HttpGet]
+<<<<<<< 304ba2c4bcafbfd5a60bca220862cfad38698c1a
         [Route("api/outage/active")]
         public async Task<IHttpActionResult> GetActive()
         {
             IEnumerable<ActiveOutageViewModel> activeOutages = await _mediator.Send(new GetActiveOutagesQuery());
+=======
+        [ResponseType(typeof(IEnumerable<Outage.Common.PubSub.OutageDataContract.ActiveOutage>))]
+        [Route("api/outage/getActive")]
+        public async Task<IHttpActionResult> GetActive()
+        {
+            IEnumerable<Outage.Common.PubSub.OutageDataContract.ActiveOutage> activeOutages = await _mediator.Send<IEnumerable<Outage.Common.PubSub.OutageDataContract.ActiveOutage>>(new GetActiveOutagesQuery());
+>>>>>>> Post Merge modifications
             return Ok(activeOutages);
         }
 
