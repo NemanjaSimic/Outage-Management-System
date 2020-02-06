4465d4c5d3a71979cf638f669f6ef296eba5d6d1
mladjo97 <mldnmilosevic@gmail.com>
2020-02-06 09:37:13 +0100

    :pencil: Add logger on topology query handler

--- OMS.Web/OMS.Web.Services/Handlers/TopologyQueryHandler.cs
+++ OMS.Web/OMS.Web.Services/Handlers/TopologyQueryHandler.cs
@@ -8,6 +8,7 @@
     using OMS.Web.Common.Mappers;
     using OMS.Web.Services.Queries;
     using OMS.Web.UI.Models.ViewModels;
+    using Outage.Common;
     using Outage.Common.UI;
     using System;
     using System.Threading;
@@ -17,13 +18,14 @@
     {
         private readonly ITopologyClient _topologyClient;
         private readonly IGraphMapper _mapper;
+        private readonly ILogger _logger;
 
-        public TopologyQueryHandler(IGraphMapper mapper)
+        public TopologyQueryHandler(IGraphMapper mapper, ILogger logger)
         {
             _mapper = mapper;
+            _logger = logger;
 
             // nece dependency injection zbog nekog unity updatea
-            // al radi ovako
             string topologyServiceAddress = AppSettings.Get<string>("topologyServiceAddress");
             _topologyClient = new TopologyClientProxy(topologyServiceAddress);
         }
@@ -34,13 +36,15 @@
             {
                 try
                 {
-                    UIModel topologyModel =  _topologyClient.GetTopology();
+                    _logger.LogInfo("[TopologyQueryHandler::GetTopologyQuery] Sending GET query to topology client.");
+                    UIModel topologyModel = _topologyClient.GetTopology();
                     OmsGraph graph = _mapper.MapTopology(topologyModel);
                     return graph;
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    throw new TopologyException();
+                    _logger.LogInfo("[TopologyQueryHandler::GetTopologyQuery] Sending GET query to topology client.");
+                    throw new TopologyException("GET query on topology failed.", ex);
                 }
             });
         }
