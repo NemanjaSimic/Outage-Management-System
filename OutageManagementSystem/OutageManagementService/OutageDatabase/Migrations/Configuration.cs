525cb60f973a4dfbcd22ef7bdd67af36678c0111
slobodanbrdar <slobodan.brdar11@gmail.com>
2020-02-12 16:38:27 +0100

    Added migration and seed method

---
+++ OutageManagementSystem/OutageManagementService/OutageDatabase/Migrations/Configuration.cs
@@ -0,0 +1,45 @@
+﻿namespace OutageDatabase.Migrations
+{
+    using Outage.Common.PubSub.OutageDataContract;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OutageContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OutageContext context)
+        {
+            ArchivedOutage archivedOutage;
+
+            long archivedId = 1;
+            archivedOutage = context.ArchivedOutages.Find(archivedId);
+
+            if (archivedOutage == null)
+            {
+                archivedOutage = new ArchivedOutage()
+                {
+                    OutageId = archivedId,
+                    ElementGid = 0x0000000a00000007,
+                    ReportTime = DateTime.Now,
+                    AffectedConsumers = new List<Consumer>(),
+                    ArchiveTime = DateTime.Now,
+                };
+
+                archivedOutage = context.ArchivedOutages.Add(archivedOutage);
+            }
+
+
+            context.SaveChanges();
+
+            context.DeleteAllData();
+            //context.Dispose();
+        }
+    }
+}
