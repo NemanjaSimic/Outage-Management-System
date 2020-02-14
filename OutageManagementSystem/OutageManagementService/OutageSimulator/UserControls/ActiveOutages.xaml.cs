20cc64899df01dbf0a47472b4b11e42cade49080
RedCrusaderJr <dimitrijemitic1996@gmail.com>
2020-02-14 08:58:44 +0100

    Outage Simulator initialization

--- OutageManagementSystem/OutageManagementService/OutageSimulator/UserControls/ActiveOutages.xaml.cs
+++ OutageManagementSystem/OutageManagementService/OutageSimulator/UserControls/ActiveOutages.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NMSTestClientUI
+namespace OMS.OutageSimulator.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ActiveOutages.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ActiveOutages : UserControl
     {
-        public MainWindow()
+        public ActiveOutages()
         {
             InitializeComponent();
         }
