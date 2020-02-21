769341cf92756e0d52472c2bbf8ef5266775d148
mladjo97 <mldnmilosevic@gmail.com>
2020-02-21 10:55:45 +0100

    Add outage lifecycle commands, handlers and update viewmodels

---
+++ OMS.Web/OMS.Web.Services/Commands/SendOutageCrewCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace OMS.Web.Services.Commands
+{
+    public class SendOutageCrewCommand : OutageLifecycleCommandBase
+    {
+        public SendOutageCrewCommand(long gid): base(gid) { }
+    }
+}
--- OMS.Web/OMS.Web.UI.Models/ViewModels/ArchivedOutageViewModel.cs
+++ OMS.Web/OMS.Web.UI.Models/ViewModels/ArchivedOutageViewModel.cs
@@ -1,17 +1,9 @@
 ﻿namespace OMS.Web.UI.Models.ViewModels
-{
-    using System;
+{    
     using System.Collections.Generic;
-    
-    public class ArchivedOutageViewModel
+
+    public class ArchivedOutageViewModel : OutageViewModel
     {
-        public long Id { get; set; }
-        public long ElementId { get; set; }
-        public DateTime ReportedAt { get; set; }
-        public DateTime ArchivedAt { get; set; }
-        public IEnumerable<ConsumerViewModel> AffectedConsumers { get; set; }
-
-
         public ArchivedOutageViewModel() => AffectedConsumers = new List<ConsumerViewModel>();
     }
 }
