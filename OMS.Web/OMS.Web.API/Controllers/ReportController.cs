1b29a45049c8bfff469188dd00dfa0b69461a4a9
mladjo97 <mldnmilosevic@gmail.com>
2020-03-06 13:36:36 +0100

    Add report query specification and options

---
+++ OMS.Web/OMS.Web.API/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿namespace OMS.Web.API.Controllers
+{
+    using MediatR;
+    using OMS.Web.UI.Models.BindingModels;
+    using System.Web.Http;
+
+    public class ReportController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public ReportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get([FromUri]ReportOptions options)
+        {
+            return Ok(options);
+        }
+    }
+}
