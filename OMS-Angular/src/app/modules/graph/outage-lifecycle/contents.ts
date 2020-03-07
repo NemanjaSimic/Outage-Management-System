b294292138dc6dba94d31fc74eb79f6a735e9bc6
thea <teodorakadic@gmail.com>
2020-03-07 10:21:48 +0100

    Fixed bug, all done ✔

--- OMS-Angular/src/app/modules/graph/outage-lifecycle/contents.ts
+++ OMS-Angular/src/app/modules/graph/outage-lifecycle/contents.ts
@@ -6,12 +6,12 @@ import {
 import { ActiveOutage, OutageLifeCycleState } from '@shared/models/outage.model';
 import { formatDate } from '@shared/utils/date';
 
-const separator = ', ';
+const separator = ',';
 
 export const generateCreatedOutageTemplate = (outage: ActiveOutage) => {
     return createdOutageTooltipContent
         .replace("[[id]]", outage.Id.toString())
-        .replace("[[defaultIsolationPoints]]", outage.DefaultIsolationPoints.map(point => `0x${point.Mrid} (${point.Id.toString(16)})`).join(separator))
+        .replace("[[defaultIsolationPoints]]", outage.DefaultIsolationPoints.map(point => `<br/>${point.Mrid} (0x${point.Id.toString(16).toUpperCase()})`).join(separator))
         .replace("[[affectedConsumers]]", outage.AffectedConsumers.length.toString())
         // .replace("[[affectedConsumers]]", outage.AffectedConsumers.map(aff => aff.Id).join(separator))
         .replace("[[state]]", OutageLifeCycleState[outage.State])
@@ -22,8 +22,8 @@ export const generateIsolatedOutageTemplate = (outage: ActiveOutage) => {
     return isolatedOutageTooltipContent
         .replace("[[id]]", outage.Id.toString())
         .replace("[[elementId]]", `0x${outage.ElementId.toString(16)}`)
-        .replace("[[defaultIsolationPoints]]", outage.DefaultIsolationPoints.map(point => `0x${point.Mrid} (${point.Id.toString(16)})`).join(separator))
-        .replace("[[optimalIsolationPoints]]", outage.OptimalIsolationPoints.map(point => `0x${point.Mrid} (${point.Id.toString(16)})`).join(separator))
+        .replace("[[defaultIsolationPoints]]", outage.DefaultIsolationPoints.map(point => `<br/>${point.Mrid} (0x${point.Id.toString(16).toUpperCase()})`).join(separator))
+        .replace("[[optimalIsolationPoints]]", outage.OptimalIsolationPoints.map(point => `<br/>${point.Mrid} (0x${point.Id.toString(16).toUpperCase()})`).join(separator))
         .replace("[[affectedConsumers]]", outage.AffectedConsumers.length.toString())
         // .replace("[[affectedConsumers]]", outage.AffectedConsumers.map(aff => aff.Id).join(separator))
         .replace("[[state]]", OutageLifeCycleState[outage.State])
@@ -34,8 +34,8 @@ export const generateRepairCrewOutageTemplate = (outage: ActiveOutage) => {
     return sendRepairCrewTooltipOutage
         .replace("[[id]]", outage.Id.toString())
         .replace("[[elementId]]", `0x${outage.ElementId.toString(16)}`)
-        .replace("[[defaultIsolationPoints]]", outage.DefaultIsolationPoints.map(point => `0x${point.Mrid} (${point.Id.toString(16)})`).join(separator))
-        .replace("[[optimalIsolationPoints]]", outage.OptimalIsolationPoints.map(point => `0x${point.Mrid} (${point.Id.toString(16)})`).join(separator))
+        .replace("[[defaultIsolationPoints]]", outage.DefaultIsolationPoints.map(point => `<br/>${point.Mrid} (0x${point.Id.toString(16).toUpperCase()})`).join(separator))
+        .replace("[[optimalIsolationPoints]]", outage.OptimalIsolationPoints.map(point => `<br/>${point.Mrid} (0x${point.Id.toString(16).toUpperCase()})`).join(separator))
         .replace("[[affectedConsumers]]", outage.AffectedConsumers.length.toString())
         // .replace("[[affectedConsumers]]", outage.AffectedConsumers.map(aff => aff.Id).join(separator))
         .replace("[[state]]", OutageLifeCycleState[outage.State])
