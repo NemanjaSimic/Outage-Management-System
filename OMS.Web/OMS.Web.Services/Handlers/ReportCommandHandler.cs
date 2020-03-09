efb95be0721b9573e7f06d8e7ed0dd8324222743
mladjo97 <mldnmilosevic@gmail.com>
2020-03-09 14:06:44 +0100

    Add generate report command and handler

---
+++ OMS.Web/OMS.Web.Services/Handlers/ReportCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace OMS.Web.Services.Handlers
+{
+    using MediatR;
+    using OMS.Web.Services.Commands;
+    using OMS.Web.UI.Models.ViewModels;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ReportCommandHandler : IRequestHandler<GenerateReportCommand, ReportViewModel>
+    {
+        public Task<ReportViewModel> Handle(GenerateReportCommand request, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                // logic
+                return new ReportViewModel { };
+            }, cancellationToken);
+        }
+    }
+}
