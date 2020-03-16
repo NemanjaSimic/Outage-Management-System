a66297cd6b71a61a7d10a545e1b873249ea60dd6
mladjo97 <mldnmilosevic@gmail.com>
2020-03-16 15:25:15 +0100

    Add outage id query

---
+++ OutageManagementSystem/OutageManagementService/OutageManagementService/Report/Queries/OutageIdSpecification.cs
@@ -0,0 +1,18 @@
+﻿namespace OutageManagementService.Report.Queries
+{
+    using OMSCommon.OutageDatabaseModel;
+    using System;
+    using System.Linq.Expressions;
+
+    public class OutageIdSpecification : OutageSpecification
+    {
+        private readonly long _id;
+
+        public OutageIdSpecification(long id)
+            => _id = id;
+
+        public override Expression<Func<OutageEntity, bool>> IsSatisfiedBy()
+            => x => x.OutageId == _id;
+        
+    }
+}
