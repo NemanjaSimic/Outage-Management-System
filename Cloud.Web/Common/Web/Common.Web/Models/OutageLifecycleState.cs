c10256da803d52a6b03fd0196b9422b23ed8e26b
teodora.kadic <teodora.kadic@uns.ac.rs>
2020-08-03 11:44:43 +0200

    Edited Enums

--- Cloud.Web/Common/Web/Common.Web/Models/OutageLifecycleState.cs
+++ Cloud.Web/Common/Web/Common.Web/Models/OutageLifecycleState.cs
@@ -1,11 +1,19 @@
-﻿namespace Common.Web.Models
+﻿using System.Runtime.Serialization;
+
+namespace Common.Web.Models
 {
+    [DataContract]
     public enum OutageLifecycleState
     {
+        [EnumMember]
         Unkown = 0,
+        [EnumMember]
         Created = 1,
+        [EnumMember]
         Isolated = 2,
+        [EnumMember]
         Repaired = 3,
+        [EnumMember]
         Archived = 4
     }
 }
--- Cloud.Web/Common/Web/Common.Web/Models/ReportType.cs
+++ Cloud.Web/Common/Web/Common.Web/Models/ReportType.cs
@@ -1,11 +1,17 @@
-﻿namespace Common.Web.Models
+﻿using System.Runtime.Serialization;
+
+namespace Common.Web.Models
 {
     // @TODO:
     // - ovo bi mozda u bazi trebalo da cuvamo ?
+    [DataContract]
     public enum ReportType
     {
+        [EnumMember]
         Total = 0,
+        [EnumMember]
         SAIFI,
+        [EnumMember]
         SAIDI
     }
 }
