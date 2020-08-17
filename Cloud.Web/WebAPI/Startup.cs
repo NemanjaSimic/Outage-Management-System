fce4e2dcab8e82c1db656ff2a00844cc16110430
Nemanja Simic <snemanja.simic@gmail.com>
2020-08-17 16:42:18 +0200

    Serialization and logger fixed on WEB Api. Topology is drawing on UI :art:

--- Cloud.Web/WebAPI/Startup.cs
+++ Cloud.Web/WebAPI/Startup.cs
@@ -1,10 +1,14 @@
 ﻿using Common.Web.Mappers;
+using Common.Web.Services.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Reflection;
 using WebAPI.Hubs;
 
 namespace WebAPI
@@ -22,6 +26,9 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -41,8 +48,16 @@ namespace WebAPI
             services.AddScoped<IOutageMapper, OutageMapper>();
             services.AddScoped<IEquipmentMapper, EquipmentMapper>();
 
-            services.AddMediatR(typeof(Startup));
+           // services.AddMediatR(typeof(Startup));
+            services.AddMediatR( new Assembly[]
+            {
+                    typeof(GetTopologyQuery).GetTypeInfo().Assembly,
+                    typeof(GetActiveOutagesQuery).GetTypeInfo().Assembly,
+                    typeof(GetArchivedOutagesQuery).GetTypeInfo().Assembly
+            });
             services.AddSignalR();
+            //var assembly = AppDomain.CurrentDomain.Load("Data");
+            //services.AddMediatR(assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
