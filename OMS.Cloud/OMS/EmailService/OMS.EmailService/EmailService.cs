8fa9f524eeb63aea18b2a64b6cd540bfa5ec8029
RedCrusaderJr <37693301+RedCrusaderJr@users.noreply.github.com>
2020-10-01 20:31:09 +0200

    uncomment RunAsync in EmailService

--- OMS.Cloud/OMS/EmailService/OMS.EmailService/EmailService.cs
+++ OMS.Cloud/OMS/EmailService/OMS.EmailService/EmailService.cs
@@ -32,38 +32,38 @@ namespace OMS.EmailService
 			Logger.LogVerbose(verboseMessage);
 		}
 
-		
 
-		/// <summary>
-		/// This is the main entry point for your service instance.
-		/// </summary>
-		/// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
-		//protected override async Task RunAsync(CancellationToken cancellationToken)
-		//{
-  //          //modo: neki while dok se ne konektuje
 
-  //          try
-  //          {
-  //              IIdleEmailClient idleEmailclient = new ImapIdleClientFactory().CreateClient();
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            //modo: neki while dok se ne konektuje
 
-  //              if (!idleEmailclient.Connect())
-  //              {
-  //                  Logger.LogError($"{baseLogString} RunAsync => idleEmailclient.Connect() returned false.");
-  //                  return;
-  //              }
+            try
+            {
+                IIdleEmailClient idleEmailclient = new ImapIdleClientFactory().CreateClient();
 
-  //              idleEmailclient.RegisterIdleHandler();
+                if (!idleEmailclient.Connect())
+                {
+                    Logger.LogError($"{baseLogString} RunAsync => idleEmailclient.Connect() returned false.");
+                    return;
+                }
 
-  //              if (!idleEmailclient.StartIdling())
-  //              {
-  //                  Logger.LogError($"{baseLogString} RunAsync => idleEmailclient.StartIdling() returned false.");
-  //                  return;
-  //              }
-  //          }
-  //          catch (Exception e)
-  //          {
-  //              Logger.LogError($"{baseLogString} RunAsync => Exception: {e.Message}");
-  //          }
-  //      }
-	}
+                idleEmailclient.RegisterIdleHandler();
+
+                if (!idleEmailclient.StartIdling())
+                {
+                    Logger.LogError($"{baseLogString} RunAsync => idleEmailclient.StartIdling() returned false.");
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{baseLogString} RunAsync => Exception: {e.Message}");
+            }
+        }
+    }
 }
